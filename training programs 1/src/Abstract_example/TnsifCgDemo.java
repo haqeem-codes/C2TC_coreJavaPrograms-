2b8e06f0f9ce50a718fcc09000a6ab50bab4b869
Lenovo <Lenovo@Rinnegan>
2025-08-20 14:27:48 +0530

    abstract_example completed

---
+++ training programs 1/src/Abstract_example/TnsifCgDemo.java
@@ -0,0 +1,14 @@
+package Abstract_example;
+
+public class TnsifCgDemo {
+	public static void main(String args[]) {
+		Trainer t=new TechTrainer();
+		t.session();
+		t.checks();
+		t=new SoftSkillTrainer();
+		t.session();
+		t.checks();
+		
+	}
+
+}
