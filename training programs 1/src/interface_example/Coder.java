a4049d12a38e6392ad24d2982d2f4563daab0f2a
Lenovo <Lenovo@Rinnegan>
2025-08-20 16:30:32 +0530

    interface examples

---
+++ training programs 1/src/interface_example/Coder.java
@@ -0,0 +1,9 @@
+package interface_example;
+
+public interface Coder {
+		
+	String wakeuptime = "5 AM";
+	
+	void codingWorks();
+	
+}
---
+++ training programs 1/src/interface_example/CoderDemo.java
@@ -0,0 +1,13 @@
+package interface_example;
+
+public class CoderDemo {
+
+	public static void main(String[] args) {
+		Coder c =new Student();
+		
+		c.codingWorks();
+		
+		c.codingWorks();
+	}
+
+}
